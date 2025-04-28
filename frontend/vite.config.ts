4d1af3d460b7ac132250e4bcafbe620a004c734b
Julian Hesberg <112935968+JulianHesberg@users.noreply.github.com>
2025-04-28 16:28:45 +0200

    revert

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pages/dist',
   plugins: [react()],
+  base: 'https://leevoi.github.io/Fumble-Funds/',
 })
